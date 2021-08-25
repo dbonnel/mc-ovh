ae98f4d25e0b3cadf1a502818d2a60290abec34a
Didier Bonnel <dbonnel@gmail.com>
2021-08-25 10:58:40 +0200

    'sh'

--- controllers/SystemController.php
+++ controllers/SystemController.php
@@ -76,7 +76,7 @@ namespace Controllers {
         public function pullProduction()
         {
            
-            $last_line = system('bash pull-prod.sh', $retval);
+            $last_line = system('/bin/bash pull-prod.sh', $retval);
             $this->view->set_var('content', "<pre> $retval</pre>");
             $this->view->set_var('page_title', 'Pull production');
             $this->view->show('main');
